ee888d40434e25a538d85a5e4accc0fe86b23467
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2018-11-03 21:24:41 +1100

    Add field "rudy-src-main" to package.json, use it to watch source files directly form the boilerplate instead of watching the compiled files in each package

--- packages/boilerplate/server/webpack.config.babel.js
+++ packages/boilerplate/server/webpack.config.babel.js
@@ -40,8 +40,9 @@ export default (env) => {
       strictExportPresence: true, // If you import something that isn't exported
       rules: [
         {
-          test: /\.js$/,
-          exclude: /node_modules/,
+          test: /\.jsx?$/,
+          // Skip node_modules, with the exception of packages in the monorepo
+          exclude: /[\\/]node_modules[\\/](?!@respond-framework)/,
           use: {
             loader: 'babel-loader',
             options: {
@@ -88,6 +89,12 @@ export default (env) => {
       extensions: isServer
         ? ['.server.js', '.js', '.css']
         : ['.browser.js', '.js', '.css'],
+      mainFields: [
+        isDev && 'rudy-src-main',
+        isClient && 'browser',
+        'module',
+        'main',
+      ].filter(Boolean)
     },
     optimization: {
       minimizer: [
