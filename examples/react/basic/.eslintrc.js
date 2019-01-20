bf5d40b61a9349a133b3e426d5241e2e45152511
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2019-01-20 12:21:57 +1100

    Chore: fix CI (#16)
    * Prettify files
    
    * Disable no-extraneous-dependencies eslint rule in the example, since there is no package.json
    
    * Boilerplate: use "rudy-src-main" entry point in production mode (to avoid dependency on library builds)
    
    * Correct value of "rudy-src-main" field in page title middleware and utils
    
    * Add "build" script to all libraries so that they are built before the CI runs the integration tests

--- examples/react/basic/.eslintrc.js
+++ examples/react/basic/.eslintrc.js
@@ -3,13 +3,14 @@ const path = require('path')
 module.exports = {
   extends: [path.resolve(__dirname, '../../../.eslintrc.js')],
   env: {
-    browser: true
+    browser: true,
   },
   rules: {
     'no-param-reassign': 0,
     'import/no-unresolved': 0,
     'import/extensions': 0,
     'react/jsx-filename-extension': 0,
-    'react/prop-types': ['error', { skipUndeclared: true }]
+    'react/prop-types': ['error', { skipUndeclared: true }],
+    'import/no-extraneous-dependencies': 0, // There is no package.json
   },
 }
