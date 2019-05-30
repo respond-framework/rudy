ac9d1ca113e6f24ba4e944f47e4789dc4c8219e9
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2019-05-30 17:49:55 +1000

    Fork npm-publish-git
    Pass --dir option to publish-to-git

--- scripts/git-release.sh
+++ scripts/git-release.sh
@@ -2,20 +2,21 @@
 
 set -e
 
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-yarn run lerna version --no-git-tag-version --no-push --allow-branch $CURRENT_BRANCH --yes
+yarn run lerna version --no-git-tag-version --no-push --allow-branch $BRANCH --yes
 git add .
 git commit -m "Publish to git"
 
-for PACKAGE in $(yarn run -s lerna changed --parseable); do
+for DIR in $(yarn run -s lerna changed --parseable); do
   (
-    cd "$PACKAGE"
-    VERSION=$(cat package.json | jq -r '.version')
-    NAME=$(cat package.json | jq -r '.name')
-    echo $NAME $VERSION $CURRENT_BRANCH
+    VERSION=$(cat "${DIR}/package.json" | jq -r '.version')
+    NAME=$(cat "${DIR}/package.json" | jq -r '.name')
 
-    yarn run prepare
-    yarn run npm-publish-git --tag "${NAME}/${CURRENT_BRANCH}/${VERSION}"
+    (
+      cd "$DIR"
+      yarn run prepare
+    )
+    yarn run npm-publish-git --dir "$DIR" --tag "${NAME}/${BRANCH}/${VERSION}"
   )
 done
