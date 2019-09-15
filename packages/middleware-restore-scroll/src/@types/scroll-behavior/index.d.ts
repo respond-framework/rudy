ac76a6136bfefff180f6d4db1355aeb4ce0630fc
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2019-09-16 09:51:13 +1000

    Add types for scroll-behavior

---
+++ packages/middleware-restore-scroll/src/@types/scroll-behavior/index.d.ts
@@ -0,0 +1,39 @@
+declare module 'scroll-behavior' {
+  export type TransitionHook = () => void
+
+  export type ScrollPosition = [number, number]
+
+  export type ScrollTarget = string | ScrollPosition
+
+  export type ShouldUpdateScroll<Context> = (
+    prevContext?: Context,
+    context?: Context,
+  ) => ScrollTarget | true | false
+
+  type ScrollBehaviorArgs<Loc, Context> = {
+    addTransitionHook: (hook: TransitionHook) => () => void
+    stateStorage: {
+      save: (location: Loc, key: string | null, value: ScrollPosition) => void
+      read: (location: Loc, key: string | null) => ScrollPosition
+    }
+    getCurrentLocation: () => Loc
+    shouldUpdateScroll?: ShouldUpdateScroll<Context>
+  }
+
+  export default class ScrollBehavior<Loc = Location, Context = Location> {
+    constructor(options: ScrollBehaviorArgs<Loc, Context>)
+
+    updateScroll: (prevContext?: Context, context?: Context) => void
+
+    registerElement: (
+      key: string,
+      element: HTMLElement,
+      shouldUpdateScroll: ShouldUpdateScroll<Context>,
+      context: Context,
+    ) => void
+
+    unregisterElement: (key: string) => void
+
+    scrollToTarget: (element: HTMLElement, target: ScrollTarget) => void
+  }
+}
