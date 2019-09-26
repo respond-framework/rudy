9ac68377930958f55ae9519880ede3ae11557b36
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2019-09-26 13:58:31 +1000

    Rudy: Improve types

--- packages/rudy/src/typescript-types.ts
+++ packages/rudy/src/typescript-types.ts
@@ -3,11 +3,19 @@
  * types (or a more complete version of them) from the entry point.
  */
 
+ /**
+  * Standard interface for redux actions which map to URLs/routes
+  */
 export interface FluxStandardRoutingAction {
   type: string;
+  params?: {},
+  query?: {},
+  state?: {},
+  hash?: string,
+  basename?: string,
 }
 
-export type LocationEntry<Action extends FluxStandardRoutingAction> = Action & {
+export type LocationEntry<Action extends FluxStandardRoutingAction> = Required<Action> & {
   location: {
     key: string,
     scene: string,
@@ -17,33 +25,63 @@ export type LocationEntry<Action extends FluxStandardRoutingAction> = Action & {
   },
 }
 
-export type DispatchedLocation<Action extends FluxStandardRoutingAction> = Action & {
+export type DispatchedLocation<Action extends FluxStandardRoutingAction> = Required<Action> & {
   prev: LocationEntry<Action> | undefined
   entries: LocationEntry<Action>[],
   index: number,
   length: number,
+  kind: "load" | "push" | "replace" | "back" | "next" | "jump" | "set" | "reset",
+  direction: "forward" | "backward",
+  n: number,
+  url: string,
+  pathname: string,
+  search: string,
+  key: string,
+  scene: string,
+  pop: boolean,
+  status: number,
 }
 
-// A routing action as it is between transformAction and the reducers
-export type DispatchedAction<Action extends FluxStandardRoutingAction> = Action & {
-  location: DispatchedLocation<Action>
+/**
+ * A routing action as it is between transformAction and the reducers
+ */
+export type DispatchedAction<Action extends FluxStandardRoutingAction> = Required<Action> & {
+  location: DispatchedLocation<Action>,
 }
 
-// The shape of the location reducer state
-export type Location<Action extends FluxStandardRoutingAction> = Action & DispatchedLocation<Action>
+/**
+ * The shape of the Rudy location reducer state
+ */
+export type Location<Action extends FluxStandardRoutingAction> = DispatchedAction<Action> & DispatchedLocation<Action>
 
+/**
+ * An instance of the Rudy API
+ */
 export interface Api<Action extends FluxStandardRoutingAction> {
   getLocation: () => Location<Action>
 }
 
+/**
+ * A Rudy request, associated with the dispatch of an FSRA
+ */
 export type Request<Action extends FluxStandardRoutingAction> = {
-  action: Action
+  action: DispatchedAction<Action>
 }
 
+/**
+ * Rudy middleware which wraps and optionally changes the behaviour of
+ * a request
+ */
 export type Middleware<Action extends FluxStandardRoutingAction> = (
   api: Api<Action>,
 ) => (request: Request<Action>, next: () => Promise<any>) => Promise<any>
 
+/**
+ * Options for a route, corresponding to FSRAs with a specific Redux action type
+ */
 export type Route = {}
 
+/**
+ * Global Rudy options
+ */
 export type Options = {}
